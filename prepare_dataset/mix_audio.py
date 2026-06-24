75a4f77d187ddeec1e8352e53a430321a6ede09d
fernandasf <fhe.ferreira11@gmail.com>
2026-06-24 19:37:15 +0000

    add mixed audio file

--- prepare_dataset/mix_audio.py
+++ prepare_dataset/mix_audio.py
@@ -5,6 +5,7 @@ import pandas as pd
 import soundfile as sf
 
 from tqdm import tqdm
+from scipy.io import wavfile
 
 
 def organize_paths(filepath, value, effect_name, suffix=""):
@@ -14,11 +15,19 @@ def organize_paths(filepath, value, effect_name, suffix=""):
     out = f"{out_folder}/{out_path}"
     return out
 
-def apply_reverb(speech_path, output_path, tfm, value):
-    tfm = sox.Transformer()
-    tfm.reverb(reverberance=value, high_freq_damping=50, room_scale=100, stereo_depth=100, pre_delay=0, wet_gain=0, wet_only=True)
-    #tfm.build('teste_imp6.wav', 'test_reverberation/imp6_sd100.wav')
-    tfm.build(speech_path, output_path)
+def apply_echo(file_path, output_path, delay_seconds=0.3, attenuation=0.5):
+    sample_rate, data = wavfile.read(file_path)
+    signal = data.astype(np.float32)
+    echos = int(delay_seconds * sample_rate)
+    
+    output_signal = np.zeros(len(signal) + echos, dtype=np.float32)
+    output_signal[:len(signal)] += signal
+    output_signal[echos:echos + len(signal)] += signal * attenuation
+    
+    # Normalize the audio back to 16-bit boundaries to prevent distortion
+    output_signal_int = np.clip(output_signal, -32768, 32767).astype(np.int16)
+    #return output_signal
+    wavfile.write(output_path, sample_rate, output_signal)
 
 
 def apply_white_noise(speech_path, output_path, target_snr_db):
@@ -35,33 +44,52 @@ def apply_white_noise(speech_path, output_path, target_snr_db):
     speech_mixed = speech + (scaling_factor * white_noise)
     speech_mixed_norm = librosa.util.normalize(speech_mixed)
     sf.write(output_path, speech_mixed_norm, sr)
-
+        
 
 if '__main__' == __name__:
     
-    #input_csv = "../lists/GPS_cmd_pyin_setup_1_test.csv"
+    input_csv = "../lists/GPS_cmd_pyin_setup_1_test.csv"
     #input_csv = "../lists/GPS_cmd_pyin_setup_1_val.csv"
-    input_csv = "../lists/GPS_cmd_pyin_setup_1_train.csv"
+    #input_csv = "../lists/GPS_cmd_pyin_setup_1_train.csv"
+
+    inputs = [
+        "../lists/GPS_cmd_pyin_setup_1_test.csv",
+        "../lists/GPS_cmd_pyin_setup_1_val.csv",
+        "../lists/GPS_cmd_pyin_setup_1_train.csv"
+    ]
+
+    for input_csv in inputs:
+
+        print(input_csv)
     
-    df = pd.read_csv(input_csv)
-
-    effect_name = "reverb"
-    #gains = [5, 10, 20]
-    values = [10, 30, 60]
+        df = pd.read_csv(input_csv)
     
-
-    mixed_filepaths = []
-    for filepath in tqdm(df['filepaths']):
-        value = np.random.choice(values)
-        output_filepath = organize_paths(filepath, value, effect_name)
-        if effect_name == "reverb":
-            apply_reverb(filepath, output_filepath, tfm, value)
-        elif effect_name == "white_noise":
-            apply_white_noise(filepath, output_filepath, value)
-
-    df["filepath_wn"] = mixed_filepaths
-    name = input_csv.replace(".csv", "_white_noise.csv")
-    df.to_csv(name, index=False)
+        effect_name = "reverb"
+        values_rev = [0.06, 0.09]
         
+        # effect_name = "white_noise"
+        # values_wn = [10, 30]
+        
+        print("Effect: ", effect_name)
+    
+        mixed_filepaths = []
+        for filepath in tqdm(df['filepaths']):
+            
+            if effect_name == "reverb":
+                value = np.random.choice(values_rev)
+                output_filepath = organize_paths(filepath, value, effect_name)
+                apply_echo(filepath, output_filepath, delay_seconds=value, attenuation=0.2)
+            elif effect_name == "white_noise":
+                value = np.random.choice(values_wn)
+                output_filepath = organize_paths(filepath, value, effect_name)
+                apply_white_noise(filepath, output_filepath, value)
+    
+            mixed_filepaths.append(output_filepath)
+    
+        df["mixed_filepath"] = mixed_filepaths
+        name = input_csv.replace(".csv", f"_{effect_name}.csv")
+        df.to_csv(name, index=False)
+        print("Save csv: ", name)
+        print("-"*80)
         
     
